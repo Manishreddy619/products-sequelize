5d052bd55b9b764f9e024bc29346f4f750e33db2
Manish Elaganti <65448066+Manishreddy619@users.noreply.github.com>
2021-09-29 12:49:54 +0200

    adding files

---
+++ src/db/index.js
@@ -0,0 +1,26 @@
+import { Sequelize } from 'sequelize';
+
+const { PGPORT, PGHOST, PGPASSWORD, PGUSER, PGDDATABASE } = process.env;
+
+const sequelize = new Sequelize(PGDDATABASE, PGUSER, PGPASSWORD, {
+	port: PGPORT,
+	host: PGHOST,
+	dialect: 'postgres',
+});
+const testDB = async () => {
+	try {
+		await sequelize.authenticate();
+		console.log('DB is authenticated');
+	} catch (error) {
+		console.log(error);
+	}
+};
+export const connectDB = async () => {
+	try {
+		await sequelize.sync();
+		console.log('DB connected');
+	} catch (error) {
+		console.log(error);
+	}
+};
+export default sequelize;
